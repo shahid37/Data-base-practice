658b5871640d94823a218e1ee91cc534593e9adc
shahid37 <happyshahid37@gmail.com>
2019-05-18 03:21:41 +0500

    insert data in reviewer table

---
+++ Inserting_Element_to_List.py
@@ -0,0 +1,43 @@
+INSERT INTO `Movie Database`.`reviewer`(
+	`rev_id`,
+    `rev_name`
+)
+VALUES (
+	`01`,
+    `ahmed`
+);
+INSERT INTO `Movie Database`.`reviewer`(
+	`rev_id`,
+    `rev_name`
+)
+VALUES (
+	`02`,
+    `alii`
+);
+INSERT INTO `Movie Database`.`reviewer`(
+	`rev_id`,
+    `rev_name`
+)
+VALUES (
+	`03`,
+    `alyas`
+);
+INSERT INTO `Movie Database`.`reviewer`(
+	`rev_id`,
+    `rev_name`
+)
+VALUES (
+	`04`,
+    `hamza`
+);
+INSERT INTO `Movie Database`.`reviewer`(
+	`rev_id`,
+    `rev_name`
+)
+VALUES (
+	`05`,
+    `fareed`
+);
+
+
+
